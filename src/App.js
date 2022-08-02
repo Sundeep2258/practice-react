bb5eef1caabf4dfb049ed61772e5784379470b56
sandeep.B <102549011+Sundeep2258@users.noreply.github.com>
2022-08-02 15:25:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import "./App.css";
 
 function App() {
+  const myArray = [
+    { id: 1, name: "sandeep", role: "software developer" },
+    { id: 2, name: "sunny", role: " developer" },
+    { id: 3, name: "sandy", role: "software " },
+    { id: 4, name: "sandeep beesu", role: "software developer" },
+    { id: 5, name: "beesu", role: "software tester" },
+    { id: 6, name: "sanp", role: "tester" },
+    { id: 7, name: "san", role: "software engineer" },
+    { id: 8, name: "sap", role: "engineer" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {myArray.map((eactItem) => {
+          return (
+            <li>
+              key={eactItem.id}
+              {eactItem.name} {}
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {myArray
+          .filter(
+            (item) =>
+              item.name.includes("san") &&
+              item.role.includes("software developer")
+          )
+          .map((item) => (
+            <li>
+              key={item.id} {item.name} {item.role}
+            </li>
+          ))}
+        {myArray
+          .filter((item) => item.role.includes("software developer"))
+          .map((item) => (
+            <li>
+              key={item.id} {item.role}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
